8bdf1e45b734e2abc467eae5ff8960fe92c57f7a
Louis Freeman <louis.freeman@gmail.com>
2021-06-02 10:52:09 +0100

    Addition of code and test cases

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -5,7 +5,18 @@
  * @returns {number} - negative of n
  */
 function asNegative(n) {
-  // ...
+//  if n is greater than 0
+  if (n>0){
+// 	return -n
+    return -n
+  }
+//   else
+  else {
+//     return n
+    return n
+  }
 }
 
-
+console.log('asNegative(20)',asNegative(20), 'should be -20')
+console.log('asNegative(-10)',asNegative(-10), 'should be -10')
+console.log('asNegative(2.3)',asNegative(2.3), 'should be -2.3')
