4d43d711b46f1b8a9158ec0a7508dac6e64a1448
Louis Freeman <louis.freeman@gmail.com>
2021-06-02 10:35:26 +0100

    Additon of JSDoc

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,5 +1,9 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Returns a negative version of a given number
+ * @param {number} n - the number to turn negative
+ * @returns {number} - negative of n
+ */
 function asNegative(n) {
   // ...
 }
